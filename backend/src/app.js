0cd2ba3ac5c0dfc098c2adc95fdf70523cd84c65
Manjeet <yadavmanjeet629@gmail.com>
2025-10-03 15:21:40 +0530

    Integrate Clerk authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express()
 
@@ -19,6 +20,7 @@ app.use(cors({
     credentials: true
 }))
 
+app.use(clerkMiddleware())
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
