dac7e3caa59b0253d773595ac4478c54b13b5c5f
abdulkalam0018 <abdulkalam754600@gmail.com>
2025-10-06 03:30:21 +0530

    adding landing page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import TodaySchedule from "./components/Dashboard/TodaySchedule";
 import CalendarSync from "./components/Calendar/Calendar";
 import DashBoard from "./pages/dashboard/DashBoard";
 // import DashBoard from "./pages/dashboard";
+import Landing from "./pages/landing/Landing";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<DashBoard />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<DashBoard />} />
         <Route path="/calendar-sync" element={<CalendarSync />} />
       </Routes>
     </Router>
