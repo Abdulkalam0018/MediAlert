bbade63566cabd84a9ae1e696ed0ee15b21e3d8d
GovindSaini1234a <Govindsaini1234a@gmail.com>
2025-10-11 05:48:21 +0530

    add frontend folder for better directory structure

---
+++ frontend/src/pages/dashboard/DashBoard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Login from "../../components/Login.jsx"
+import DashboardHeader from "../../components/dashboard/DashboardHeader";
+import AdherenceStats from "../../components/dashboard/AdherenceStats";
+import TodaySchedule from "../../components/Dashboard/TodaySchedule";
+import AIHealthAssistant from "../../components/Dashboard/AIHealthAssistant";
+import "../../components/Dashboard/Dashboard.css";
+
+export default function Dashboard() {
+  return (
+    <div style={{ display: 'flex', position: 'relative' }}>
+      <div style={{ position: 'fixed', right: '1rem', top: '1rem', zIndex: 1000, transform: 'scale(1.8)', transformOrigin: 'top right' }}>
+        <Login />
+      </div>
+      
+      <div className="dashboard" style={{ width: '100%' }}>
+        <DashboardHeader />
+        <AdherenceStats/>
+        <TodaySchedule />
+        {/* <RecentActivity /> */}
+        <AIHealthAssistant />
+      </div>
+    </div>
+  );
+}
